e7da31283172d8663c702b112efc83f71742bf48
Maksim Sutormine <mahiroruhiro@gmail.com>
2024-11-27 20:27:12 +0300

    yj

--- game.py
+++ game.py
@@ -1,127 +1,60 @@
-import pygame
-import time
-import random
- 
-pygame.init()
- 
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
- 
-dis_width = 600
-dis_height = 400
- 
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Pythonist')
- 
-clock = pygame.time.Clock()
- 
-snake_block = 10
-snake_speed = 15
- 
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
- 
- 
-def Your_score(score):
-    value = score_font.render("Your Score: " + str(score), True, yellow)
-    dis.blit(value, [0, 0])
- 
- 
- 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
- 
- 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
- 
- 
-def gameLoop():
-    game_over = False
-    game_close = False
- 
-    x1 = dis_width / 2
-    y1 = dis_height / 2
- 
-    x1_change = 0
-    y1_change = 0
- 
-    snake_List = []
-    Length_of_snake = 1
- 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
- 
-    while not game_over:
- 
-        while game_close == True:
-            dis.fill(blue)
-            message("You Lost! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1)
-            pygame.display.update()
- 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
- 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
- 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
- 
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
- 
-        our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
- 
-        pygame.display.update()
- 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
- 
-        clock.tick(snake_speed)
- 
-    pygame.quit()
-    quit()
- 
- 
-gameLoop()
+#Вызов библиотек
+from tkinter import *
+from random import randint
+
+class Game:
+    #Инициализация
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.snake_coords = [[14, 14]]
+        self.apple_coords = [randint(0, 29) for i in range(2)]
+        self.vector = {"Up":(0,-1), "Down":(0, 1), "Left": (-1,0), "Right": (1, 0)}
+        self.direction = self.vector["Right"]
+        self.canvas.focus_set()
+        self.canvas.bind("<KeyPress>", self.set_direction)
+        self.GAME()
+    #Метод для нового положения "Яблока"
+    def set_apple(self):
+        self.apple_coords = [randint(0, 29) for i in range(2)]
+        #Условие, для того чтобы яблоко не лежало на змейке
+        if self.apple_coords in self.snake_coords:
+            self.set_apple()
+    #Установка нового направления змейки
+    def set_direction(self, event):
+      #Условие, которое проверяет нажатие кнопки
+        if event.keysym in self.vector:
+            self.direction = self.vector[event.keysym]
+    #Отрисовка игры
+    def draw(self):
+        self.canvas.delete(ALL)
+        x_apple, y_apple = self.apple_coords
+        self.canvas.create_rectangle(x_apple*10, y_apple*10, (x_apple+1)*10, (y_apple+1)*10, fill="red", width=0)
+        for x, y in self.snake_coords:
+            self.canvas.create_rectangle(x*10, y*10, (x+1)*10, (y+1)*10, fill="green", width=0)
+    #Метод, который возращает координаты на интервале [0, 29]
+    @staticmethod
+    def coord_check(coord):
+        return 0 if coord > 29 else 29 if coord < 0 else coord
+    #Алгоритм "Оторванный Хвост\Логика игры"       
+    def GAME(self):
+        self.draw()
+        x,y = self.snake_coords[0]
+        x += self.direction[0]; y += self.direction[1]
+        x = self.coord_check(x)
+        y = self.coord_check(y)
+        if x == self.apple_coords[0] and y == self.apple_coords[1]:
+            self.set_apple()
+        elif [x, y] in self.snake_coords:
+            self.snake_coords = []
+        else:
+            self.snake_coords.pop()
+        self.snake_coords.insert(0, [x,y])
+        self.canvas.after(100, self.GAME)
+        
+        
+#Каркас игры
+root = Tk()
+canvas = Canvas(root, width=300, height=300, bg="black")
+canvas.pack()
+game = Game(canvas)
+root.mainloop()
